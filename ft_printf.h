20823ce0514794287d5aefdb71807b786a98ad70
Andoni Arrien Martin <aarrien-@c3r3s3.42urduliz.com>
2022-10-06 13:31:02 +0200

    estructura de ft_printf

---
+++ ft_printf.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aarrien- <aarrien-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/30 08:57:58 by aarrien-          #+#    #+#             */
+/*   Updated: 2022/10/06 12:37:34 by aarrien-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdio.h>
+# include <unistd.h>
+#include <stdarg.h> // va_start ,va_arg ,va_copy ,va_end
+
+int		ft_printf(char const *s, ...);
+
+void	ft_putstr(char const *str);
+
+#endif
